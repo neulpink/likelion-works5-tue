4e55b978b7ec9f60bf19231170eb2a712df762ed
dev-heeham <dev.heeham@gmail.com>
2020-10-20 16:51:22 +0900

    Week6 Part 1

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+
+    author = models.CharField(max_length=100)
+    body = models.TextField()
+    created_at = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.author}: {self.body}'
--- posts/views.py
+++ posts/views.py
@@ -1,35 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
 
-class User:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-
-# def index(request):
-#     context = {
-#         'post': {
-#             'author': '_heeham',
-#             'body': '나의 첫 #Django Project...'
-#         },
-#         'numbers': [1, 2, 3],
-#         'user': User('heeham', 29)
-#     }
-
-#     return render(request, 'posts/index.html', context)
-
 def index(request):
+    posts = Post.objects.all()
     context = {
-        'posts': [
-            {'author': 'hv_nara', 'body': 'Amanproject, Kwon nara'},
-            {'author': '_heeham', 'body': '첫 #halloween'},
-            {'author': 'd_a___m_i', 'body': '오랜만이서'},
-            {'author': 'iamdoo2', 'body': 'PPF 붙혀주는 여자친구 #심지어잘함'},
-        ]
+        'posts': posts
     }
 
     return render(request, 'posts/index.html', context)
