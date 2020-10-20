b9eb440375392628fb96227c06b74bc1f90263b4
dev-heeham <dev.heeham@gmail.com>
2020-10-20 17:28:56 +0900

    CR

--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,35 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from .models import Post
+from django.utils import timezone
+
 
 # Create your views here.
 
 
 def index(request):
     posts = Post.objects.all()
-    context = {
-        'posts': posts
-    }
+
+    context = {'posts': posts}
 
     return render(request, 'posts/index.html', context)
+
+
+def detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+
+    context = {'post': post}
+
+    return render(request, 'posts/detail.html', context)
+
+
+def new(request):
+    return render(request, 'posts/new.html')
+
+
+def create(request):
+    author = request.POST['author']
+    body = request.POST['body']
+    post = Post(author=author, body=body, created_at=timezone.now())
+    post.save()
+
+    return redirect('posts:detail', post_id=post.id)
