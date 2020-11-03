a54a66cd5ed0afa5c6208ecb0117e25b93f13bd8
dev-heeham <dev.heeham@gmail.com>
2020-11-03 10:26:47 +0900

    Implement OneToMany

--- posts/models.py
+++ posts/models.py
@@ -1,13 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class Post(models.Model):
 
-    author = models.CharField(max_length=100)
+    # author = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
     body = models.TextField()
     created_at = models.DateTimeField()
 
     def __str__(self):
-        return f'{self.author}: {self.body}'
+        if self.user:
+            return f'{self.user.get_username()}: {self.body}'
+
+        return f'{self.body}'
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Post
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -22,36 +23,54 @@ def detail(request, post_id):
     return render(request, 'posts/detail.html', context)
 
 
+@login_required
 def new(request):
+
     return render(request, 'posts/new.html')
 
 
+@login_required
 def create(request):
-    author = request.POST['author']
+
+    user = request.user
     body = request.POST['body']
-    post = Post(author=author, body=body, created_at=timezone.now())
+    post = Post(user=user, body=body, created_at=timezone.now())
     post.save()
 
     return redirect('posts:detail', post_id=post.id)
 
 
+@login_required
 def edit(request, post_id):
-    post = Post.objects.get(id=post_id)
+    try:
+        post = Post.objects.get(id=post_id, user=request.user)
+    except Post.DoesNotExist:
+        return redirect('posts:index')
+
     context = {'post': post}
     return render(request, 'posts/edit.html', context)
 
 
+@login_required
 def update(request, post_id):
-    post = Post.objects.get(id=post_id)
-    post.author = request.POST['author']
+    try:
+        post = Post.objects.get(id=post_id, user=request.user)
+    except Post.DoesNotExist:
+        return redirect('posts:index')
+
     post.body = request.POST['body']
     post.save()
 
     return redirect('posts:detail', post_id=post.id)
 
 
+@login_required
 def delete(request, post_id):
-    post = Post.objects.get(id=post_id)
+    try:
+        post = Post.objects.get(id=post_id, user=request.user)
+    except Post.DoesNotExist:
+        return redirect('posts:index')
+
     post.delete()
 
     return redirect('posts:index')
